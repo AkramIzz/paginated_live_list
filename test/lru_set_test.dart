a7aa51fb4edb72b451e39495fa4c9dfa21f3dd26
Akram Izzeldin <akram.izz.kh@gmail.com>
2022-01-05 22:23:02 +0200

    format code

--- test/lru_set_test.dart
+++ test/lru_set_test.dart
@@ -9,7 +9,10 @@ void main() {
     expect(lruSet.length, 1);
   });
   test('can test whether an element is in the LruSet or not', () {
-    final lruSet = LruSet<int>(5)..put(1)..put(2)..put(3);
+    final lruSet = LruSet<int>(5)
+      ..put(1)
+      ..put(2)
+      ..put(3);
     expect(lruSet.contains(1), true);
     expect(lruSet.contains(4), false);
   });
@@ -25,18 +28,26 @@ void main() {
     expect(lruSet.length, 3);
   });
   test('adding an element to a full LruSet removes an item and returns it', () {
-    final lruSet = LruSet<int>(3)..put(1)..put(2)..put(3);
+    final lruSet = LruSet<int>(3)
+      ..put(1)
+      ..put(2)
+      ..put(3);
     expect(lruSet.length, 3);
     expect(lruSet.put(4), 1);
     expect(lruSet.length, 3);
   });
   test('adding an item for a second time does not affect the set', () {
     final lruSet = LruSet<int>(3);
-    lruSet..put(1)..put(1);
+    lruSet
+      ..put(1)
+      ..put(1);
     expect(lruSet.length, 1);
   });
   test('adding an already existing item updates it in the LRU list', () {
-    final lruSet = LruSet<int>(3)..put(1)..put(2)..put(3);
+    final lruSet = LruSet<int>(3)
+      ..put(1)
+      ..put(2)
+      ..put(3);
     lruSet.put(1);
     expect(lruSet.put(4), 2);
     expect(lruSet.put(5), 3);
